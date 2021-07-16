a859f8eb8ddf5f650f8b29b71ac87462f003ab90
Bryant Wooters <Jesusrox45@gmail.com>
2021-07-16 19:49:40 -0400

    Completed header, notes, and footer component

---
+++ src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Notes(){
+    return (
+        <div className="note">
+            <h1>This is a title</h1>
+            <p>This is a note</p>
+        </div>
+    )
+}
+
+
+
+
+export default Notes;
